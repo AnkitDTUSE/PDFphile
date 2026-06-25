0715e2ee2598dd79410094e5d6421d26d11dd796
ankit <ankitpanchal41106@gmail.com>
2026-06-25 18:19:52 +0530

    Production changes done

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -4,7 +4,7 @@ import axios from "axios";
 import "./index.css";
 import App from "./App.jsx";
 
-axios.defaults.baseURL = "http://localhost:3000";
+axios.defaults.baseURL = "https://pdfphilebackend.onrender.com";
 axios.defaults.withCredentials = true;
 
 createRoot(document.getElementById("root")).render(<App />);
